533ecde8e2e5ebac004b2163d557680688efdf88
Eduardo Corzo <edukorzo@gmail.com>
2021-01-05 15:33:44 +0100

    Checkpoint (#3)
    * Adds a new checkpoint input at the end
    
    * Clean up

--- src/CheckpointListForm.js
+++ src/CheckpointListForm.js
@@ -3,9 +3,26 @@ import { Form, Button } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 import CheckpointForm from './CheckpointForm.js';
 
+export function createCheckpointForms(checkpoints, onChange) {
+  let checkpointId,
+    checkpointforms = [];
+
+  for (checkpointId in checkpoints) {
+    if (Object.prototype.hasOwnProperty.call(checkpoints, checkpointId)) {
+      checkpointforms.push(<CheckpointForm key= {checkpointId} checkpointId={checkpointId} onChange={onChange}/>);
+    }
+  }
+  
+  if (checkpoints[checkpointId] !== '') {
+    checkpointforms.push(<CheckpointForm key= {checkpointId + 1} checkpointId={checkpointId + 1} onChange={onChange}/>);
+  }
+
+  return checkpointforms;
+}
+
 export default function CheckpointListForm(props) {
   const [title, setTitle] = useState('');
-  const [checkpoints, setCheckpoints] = useState({});
+  const [checkpoints, setCheckpoints] = useState({ 0: '', 1: '' });
 
   function submited(e) {
     e.preventDefault();
@@ -16,17 +33,18 @@ export default function CheckpointListForm(props) {
     let newCheckpoints = { ...checkpoints };
 
     newCheckpoints[id] = editedCheckpoint;
-
+    
     setCheckpoints(newCheckpoints);
   }
+
+  let checkpointForms = createCheckpointForms(checkpoints, onChange);
 
   return (
     <div>
       <h1>Create list</h1>
       <Form className='CheckpointListForm' onSubmit={submited}>
         <Form.Control placeholder='Title' maxLength='20' value={title} onChange={e => setTitle(e.target.value)} name='title'></Form.Control>
-        <CheckpointForm checkpointId='0' onChange={onChange}/>
-        <CheckpointForm checkpointId='1' onChange={onChange}/>
+        {checkpointForms}
         <Button type='submit'>Done</Button>
       </Form>
     </div>
