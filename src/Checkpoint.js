404ca65e8ec8bb011a52ff483a5252e433238da8
Eduardo Corzo <edukorzo@gmail.com>
2021-01-22 19:27:10 +0100

    Improving checkpoint

--- src/Checkpoint.js
+++ src/Checkpoint.js
@@ -17,9 +17,9 @@ export default function Checkpoint(props) {
   }
 
   return (
-    <div>
-      <h2 key={props.checkpointId} className='d-inline'>{props.title}</h2>
-      <Check color={color} size={90} title='check' className='d-inline' onClick={changeStatus}/>
+    <div className='Checkpoint row'>
+      <h2 key={props.checkpointId} className='col-sm'>{props.title}</h2>
+      <Check color={color} size={90} title='check' className='col-sm' onClick={changeStatus}/>
     </div>
   );
 }
